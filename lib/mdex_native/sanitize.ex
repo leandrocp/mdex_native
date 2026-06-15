9252130f1beebff449317033a10596c2003a5da8
Leandro Pereira <leandro@leandro.io>
2026-06-15 18:46:20 -0400

    chore: e2e mdex tests (#21)

--- lib/mdex_native/sanitize.ex
+++ lib/mdex_native/sanitize.ex
@@ -1,11 +1,51 @@
 defmodule MDExNative.Sanitize do
   @moduledoc false
 
+  @set_add_rm_options [
+    :tags,
+    :clean_content_tags,
+    :tag_attributes,
+    :tag_attribute_values,
+    :set_tag_attribute_values,
+    :generic_attribute_prefixes,
+    :generic_attributes,
+    :url_schemes,
+    :allowed_classes
+  ]
+
   def normalize(nil), do: nil
   def normalize(:default), do: :clean
   def normalize(:clean), do: :clean
-  def normalize({:custom, _custom} = sanitize), do: sanitize
+  def normalize({:custom, custom}), do: {:custom, normalize_custom(custom)}
   def normalize(options) when is_list(options), do: adapt_options(options)
+
+  defp normalize_custom(custom) do
+    Map.merge(default_custom(), custom, fn
+      key, default, value when key in @set_add_rm_options -> Map.merge(default, value)
+      _key, _default, value -> value
+    end)
+  end
+
+  defp set_add_rm, do: %{set: nil, add: nil, rm: nil}
+
+  defp default_custom do
+    %{
+      tags: set_add_rm(),
+      clean_content_tags: set_add_rm(),
+      tag_attributes: set_add_rm(),
+      tag_attribute_values: set_add_rm(),
+      set_tag_attribute_values: set_add_rm(),
+      generic_attribute_prefixes: set_add_rm(),
+      generic_attributes: set_add_rm(),
+      url_schemes: set_add_rm(),
+      allowed_classes: set_add_rm(),
+      link_rel: nil,
+      url_relative: nil,
+      strip_comments: nil,
+      id_prefix: nil,
+      filter_style_properties: nil
+    }
+  end
 
   defp adapt_options(options) do
     {:custom,
